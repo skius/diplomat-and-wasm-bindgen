b7ed7f2f380486157b053d3156fd71f23c8728c8
Niels Saurer <me@nielssaurer.com>
2025-05-19 20:19:04 +0200

    format

--- lib-js/api/diplomat-wasm.mjs
+++ lib-js/api/diplomat-wasm.mjs
@@ -3,27 +3,27 @@ import {readString8} from './diplomat-runtime.mjs'
 import * as wbg from '../wbg/lib_ffi.js'
 
 const imports = {
-    env: {
-        diplomat_console_debug_js(ptr, len) {
-            console.debug(readString8(wasm, ptr, len));
-        },
-        diplomat_console_error_js(ptr, len) {
-            console.error(readString8(wasm, ptr, len));
-        },
-        diplomat_console_info_js(ptr, len) {
-            console.info(readString8(wasm, ptr, len));
-        },
-        diplomat_console_log_js(ptr, len) {
-            console.log(readString8(wasm, ptr, len));
-        },
-        diplomat_console_warn_js(ptr, len) {
-            console.warn(readString8(wasm, ptr, len));
-        },
-        diplomat_throw_error_js(ptr, len) {
-            throw new Error(readString8(wasm, ptr, len));
-        }
+env: {
+    diplomat_console_debug_js(ptr, len) {
+        console.debug(readString8(wasm, ptr, len));
     },
-    wbg: wbg.__wbg_get_imports().wbg
+    diplomat_console_error_js(ptr, len) {
+        console.error(readString8(wasm, ptr, len));
+    },
+    diplomat_console_info_js(ptr, len) {
+        console.info(readString8(wasm, ptr, len));
+    },
+    diplomat_console_log_js(ptr, len) {
+        console.log(readString8(wasm, ptr, len));
+    },
+    diplomat_console_warn_js(ptr, len) {
+        console.warn(readString8(wasm, ptr, len));
+    },
+    diplomat_throw_error_js(ptr, len) {
+        throw new Error(readString8(wasm, ptr, len));
+    }
+},
+wbg: wbg.__wbg_get_imports().wbg
 }
 
 let wasm = await wbg.default(imports)
